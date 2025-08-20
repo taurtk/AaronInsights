73f48417ce233f7741bd55bf09aabcad7400b50b
taurtk <taurtk@gmail.com>
2025-08-20 16:39:12 +0530

    updated version

--- utils/deepseek_client.py
+++ utils/deepseek_client.py
@@ -7,11 +7,7 @@ import pandas as pd
 
 class DeepSeekClient:
     def __init__(self):
-        # Try Streamlit secrets first, then environment variables
-        try:
-            api_key = st.secrets["DEEPSEEK_API_KEY"]
-        except:
-            api_key = os.getenv("DEEPSEEK_API_KEY", "736f180b-6d5b-4294-8695-d14ffd734eff")
+        api_key = os.getenv("DEEPSEEK_API_KEY")
         
         self.client = openai.OpenAI(
             api_key=api_key,
